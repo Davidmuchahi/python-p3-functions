259ebf4765300a998704f05d36146a6908d499af
David Muchahi <muchahi853@gmail.com>
2025-04-12 13:10:12 +0300

    Finished lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet("programmer")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default("John")
 
 def add(num1, num2):
-    pass
+    return num1+num2
+
+
+sum=add(1,3)
+print(sum)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
